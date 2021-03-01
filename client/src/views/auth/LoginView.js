a050beab7f17e5f6c4a1f8ee572cf7fec52dbf83
CITIZENDOT <appaji12368@gmail.com>
2021-03-01 11:38:43 +0530

    Fixed Build Errors

--- client/src/views/auth/LoginView.js
+++ client/src/views/auth/LoginView.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import * as Yup from "yup";
 import { Formik } from "formik";
 import {
