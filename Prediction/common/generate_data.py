2a4a9967376ccbd4e937dc747426fba2d8d5a6a1
CITIZENDOT <52322531+CITIZENDOT@users.noreply.github.com>
2021-07-17 21:22:17 +0530

    ✨ Introduced Trading algorithms

--- Prediction/common/generate_data.py
+++ Prediction/common/generate_data.py
@@ -3,6 +3,7 @@ import yfinance as yf
 
 def get_data(symbol="AAPL", period="1mo", interval="5m", file_name="data.csv"):
     data = yf.download(tickers=symbol, period=period, interval=interval)
+    print(data)
     data.to_csv(file_name)
 
 
